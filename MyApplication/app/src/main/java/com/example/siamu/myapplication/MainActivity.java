196b8d221de4b736cd8a999ee4ae1f7330e50e3a
Siam Umar Hussain <siamumar@ucsd.edu>
2018-02-28 16:50:55 -0800

    update for new Android Studio

---
+++ MyApplication/app/src/main/java/com/example/siamu/myapplication/MainActivity.java
@@ -0,0 +1,243 @@
+package com.example.siamu.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    Button btnOn, buttonTrain, buttonReset;
+    TextView reps, w_reps, info;
+    Handler bluetoothIn;
+
+    final int handlerState = 0;                        //used to identify handler message
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private StringBuilder recDataString = new StringBuilder();
+
+    private ConnectedThread mConnectedThread;
+
+    // SPP UUID service - this should work for most devices
+    private static final UUID BTMODULEUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    // String for MAC address
+    private static String address;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        //Link the buttons and textViews to respective views
+        btnOn = (Button) findViewById(R.id.buttonOn);
+        buttonTrain = (Button) findViewById(R.id.buttonTrain);
+        buttonReset = (Button) findViewById(R.id.buttonReset);
+        reps = (TextView) findViewById(R.id.reps);
+        w_reps = (TextView) findViewById(R.id.w_reps);
+        info = (TextView) findViewById(R.id.info);
+
+        bluetoothIn = new Handler() {
+            public void handleMessage(android.os.Message msg) {
+                if (msg.what == handlerState) {                                     //if message is what we want
+                    String readMessage = (String) msg.obj;
+                    String[] tokens = readMessage.split("-");
+                    if (tokens[0].matches("t")) {
+                        info.setText(/*"Max speed set to "+*/tokens[1]);
+                    }
+                    else {
+                        if(tokens.length >= 1) reps.setText(tokens[0]);
+                        if(tokens.length >= 2) w_reps.setText(tokens[1]);
+                    }
+                }
+            }
+        };
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter();       // get Bluetooth adapter
+        checkBTState();
+
+
+        btnOn.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                if(address == null) {
+                    Toast.makeText(getBaseContext(), "Please connect to Phase Gear", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mConnectedThread.write("1");
+                reps.setText("0");
+                w_reps.setText("0");
+            }
+        });
+
+        buttonTrain.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                if(address == null) {
+                    Toast.makeText(getBaseContext(), "Please connect to Phase Gear", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mConnectedThread.write("2");
+                reps.setText("0");
+                w_reps.setText("0");
+                info.setText("Please perform 10 training reps");
+            }
+        });
+
+        buttonReset.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                if(address == null) {
+                    Toast.makeText(getBaseContext(), "Please connect to Phase Gear", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mConnectedThread.write("3");
+                Toast.makeText(getBaseContext(), "Speed reset", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+
+        return  device.createRfcommSocketToServiceRecord(BTMODULEUUID);
+        //creates secure outgoing connecetion with BT device using UUID
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //Get MAC address from DeviceListActivity via intent
+        Intent intent = getIntent();
+
+        //Get the MAC address from the DeviceListActivty via EXTRA
+        address = intent.getStringExtra(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+        if(address == null) return;
+
+        //create device and set the MAC address
+        BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+        try {
+            btSocket = createBluetoothSocket(device);
+        } catch (IOException e) {
+            Toast.makeText(getBaseContext(), "Socket creation failed", Toast.LENGTH_LONG).show();
+        }
+        // Establish the Bluetooth socket connection.
+        try
+        {
+            btSocket.connect();
+        } catch (IOException e) {
+            try
+            {
+                btSocket.close();
+            } catch (IOException e2)
+            {
+                //insert code to deal with this
+            }
+        }
+        mConnectedThread = new ConnectedThread(btSocket);
+        mConnectedThread.start();
+
+        //I send a character when resuming.beginning transmission to check device is connected
+        //If it is not an exception will be thrown in the write method and finish() will be called
+        mConnectedThread.write("x");
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if(address == null) return;
+        try
+        {
+            //Don't leave Bluetooth sockets open when leaving activity
+            btSocket.close();
+        } catch (IOException e2) {
+            //insert code to deal with this
+        }
+    }
+
+    //Checks that the Android device Bluetooth is available and prompts to be turned on if off
+    private void checkBTState() {
+
+        if(btAdapter==null) {
+            Toast.makeText(getBaseContext(), "Device does not support bluetooth", Toast.LENGTH_LONG).show();
+        } else {
+            if (btAdapter.isEnabled()) {
+            } else {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, 1);
+            }
+        }
+    }
+
+    //create new class for connect thread
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        //creation of the connect thread
+        public ConnectedThread(BluetoothSocket socket) {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                //Create I/O streams for connection
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[256];
+            int bytes;
+
+            // Keep looping to listen for received messages
+            while (true) {
+                try {
+                    bytes = mmInStream.read(buffer);            //read bytes from input buffer
+                    String readMessage = new String(buffer, 0, bytes);
+                    // Send the obtained bytes to the UI Activity via handler
+                    bluetoothIn.obtainMessage(handlerState, bytes, -1, readMessage).sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        //write method
+        public void write(String input) {
+            byte[] msgBuffer = input.getBytes();           //converts entered String into bytes
+            try {
+                mmOutStream.write(msgBuffer);                //write bytes over BT connection via outstream
+            } catch (IOException e) {
+                //if you cannot write, close the application
+                Toast.makeText(getBaseContext(), "Connection Failure", Toast.LENGTH_LONG).show();
+                finish();
+
+            }
+        }
+    }
+
+    public void showBTdevices(View view) {
+        Intent intent = new Intent(this, DeviceListActivity.class);
+        startActivity(intent);
+    }
+
+
+}
